9f99b454d0d6a90d3dc435d27e59f8c9f56a2cc9
Tuulia Arvola <112947716+tuuliaarvola@users.noreply.github.com>
2026-04-02 23:20:21 +0300

    3.1 of the tutorial done

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //for constantly calling SpawnObstacle
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle ()
+    {
+        //for making clones of the obstacle
+        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+    }
+}
