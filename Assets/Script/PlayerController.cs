79b974d2ea13b1ed58c50a5cb07e9292c634f0e5
Tuulia Arvola <112947716+tuuliaarvola@users.noreply.github.com>
2026-04-02 22:57:40 +0300

    Done to 3.1.7

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    //float is for desimile numbers
+    public float jumpForce = 10;
+    public float gravityModifier;
+    //bool is a variable which is either true or false
+    public bool isOnGround = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //input for spacebar
+        //&& = and
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround= false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
